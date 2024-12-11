f3b3b017300f753f9b346c0a941c225aa7de2d89
Andrei Markeev <andrei-markeev@users.noreply.github.com>
2024-12-11 04:22:02 +0200

    stop emulator in graceful shutdown, also recovering after crashes when build is stuck in pending state

--- src/.init.lua
+++ src/.init.lua
@@ -14,8 +14,44 @@ function OnHttpRequest()
     end
 end
 
-LaunchBrowser()
+function OnServerStop()
+    if path.exists('.pebble/qemu_control') then
+        print("Try closing Qemu...")
+    
+        local sockfd = unix.socket()
+        assert(unix.connect(sockfd, ResolveIp('127.0.0.1'), 12346))
+        assert(unix.write(sockfd, 'quit\n'))
+        assert(unix.close(sockfd));
+    
+        assert(unix.unlink('.pebble/qemu_control'))
+    end
+end
 
 if path.exists('.pebble/qemu_control') then
     assert(unix.unlink('.pebble/qemu_control'))
 end
+
+local build_db_filename = '.pebble/builds/db.json';
+if path.exists(build_db_filename) then
+    local builds = Slurp(build_db_filename)--[[@as any]]
+
+    if builds ~= nil then
+        builds = assert(DecodeJson(builds))--[[@as any]]
+        if builds[1].state == 1 then
+            table.remove(builds, 1)
+        end
+        assert(Barf(build_db_filename, EncodeJson(builds)));
+
+        local DownloadBundle = require('DownloadBundle')
+        local container_dir = DownloadBundle.getContainerDir()
+        local rootfs_dir = path.join(container_dir, 'rootfs')
+        local container_app_dir = path.join(rootfs_dir, 'pebble/app');
+        local assembled_dir = path.join(rootfs_dir, 'pebble/assembled');
+        if path.exists(container_app_dir) then
+            unix.rmrf(container_app_dir);
+            unix.rmrf(assembled_dir);
+        end
+    end
+end
+
+LaunchBrowser()
